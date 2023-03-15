d091c6ce5b456b3eb309ac4c2074ebac8ca7dd12
Harshavardhana <harsha@minio.io>
2023-03-15 12:43:24 -0700

    cleanup old code, change license

--- README.md
+++ README.md
@@ -1,34 +1,37 @@
-### minio-webhook
+# Audit/Logger Webhook
 
-minio-webhook listens for webhook events from MinIO server and logs the event to a log file.
+`webhook` listens for incoming events from MinIO server and logs these events to a log file.
 
 Usage:
 ```
-minio-webhook <logfile>
+webhook --log-file <logfile>
 ```
 
-Environment variables:
-* MINIO_WEBHOOK_AUTH_TOKEN: Authorization token to be used by minio server for sending events
-* MINIO_WEBHOOK_PORT: Listening port (Default 8080)
+Environment only settings:
 
-The minio-webhook service can be setup as a systemd service using the provided minio-webhook.service file
+| ENV                | Description                                                        |
+|--------------------|--------------------------------------------------------------------|
+| WEBHOOK_AUTH_TOKEN | Authorization token optional to authenticate/trust incoming events |
 
-To send access/audit logs from MinIO server, please configure MinIO using the command:
+The webhook service can be setup as a systemd service using the `webhook.service` shipped with
+this project
+
+To send Audit logs from MinIO server, please configure MinIO using the command:
 ```
 mc admin config set myminio audit_webhook endpoint=http://webhookendpoint:8080 auth_token=webhooksecret
 ```
 
-To send MinIO's error logs to minio-webhook, please configure MinIO using the command:
+To send server logs from MinIO server, please configure MinIO using the command:
 ```
 mc admin config set myminio logger_webhook endpoint=http://webhookendpoint:8081 auth_token=webhooksecret
 ```
 
-Note: audit_webhook and logger_webhook should *not* be configured to send events to the same minio-webhook instance.
+> NOTE: audit_webhook and logger_webhook should *not* be configured to send events to the same webhook instance.
 
 Logs can be rotated using the standard logrotate tool. You can provide the postrotate command such that
-minio-webhook writes to a new log file after log rotation.
+webhook writes to a new log file after log rotation.
 ```
 postrotate
-	systemctl reload minio-webhook
+	systemctl reload webhook
 endscript
 ```
--- main.go
+++ main.go
@@ -1,65 +1,91 @@
+// Copyright (c) 2015-2023 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"flag"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
+
+	"github.com/minio/pkg/env"
 )
 
-var authToken = os.Getenv("MINIO_WEBHOOK_AUTH_TOKEN")
-var port = os.Getenv("MINIO_WEBHOOK_PORT")
+var (
+	logFile   string
+	address   string
+	authToken = env.Get("WEBHOOK_AUTH_TOKEN", "")
+)
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: minio-webhook filename.log")
-		os.Exit(1)
+	flag.StringVar(&logFile, "log-file", "", "path to the file where webhook will log incoming events")
+	flag.StringVar(&address, "address", ":8080", "bind to a specific ADDRESS:PORT, ADDRESS can be an IP or hostname")
+
+	flag.Parse()
+
+	if logFile == "" {
+		log.Fatalln("--log-file must be specified")
 	}
-	if port == "" {
-		port = "8080"
-	}
-	logFilePath := os.Args[1]
-	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0640)
+
+	l, err := os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0640)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var logFileMu sync.Mutex
+
+	var mu sync.Mutex
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGHUP)
 
 	go func() {
 		for _ = range sigs {
-			logFileMu.Lock()
-			logFile.Close()
-			logFile, err = os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0640)
+			mu.Lock()
+			l.Sync()  // flush to disk any temporary buffers.
+			l.Close() // then close the file, before rotation.
+			l, err = os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0640)
 			if err != nil {
 				log.Fatal(err)
 			}
-			logFileMu.Unlock()
+			mu.Unlock()
 		}
 	}()
 
-	err = http.ListenAndServe(":"+port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	err = http.ListenAndServe(address, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if authToken != "" {
 			if authToken != r.Header.Get("Authorization") {
+				http.Error(w, "authorization header missing", http.StatusBadRequest)
 				return
 			}
 		}
 		switch r.Method {
-		case "POST":
-			data, err := ioutil.ReadAll(r.Body)
+		case http.MethodPost:
+			mu.Lock()
+			_, err := io.Copy(l, r.Body)
 			if err != nil {
+				mu.Unlock()
 				return
 			}
-			logFileMu.Lock()
-			logFile.Write(data)
-			logFile.WriteString("\n")
-			logFileMu.Unlock()
+			l.WriteString("\n")
+			mu.Unlock()
 		default:
 		}
 	}))
